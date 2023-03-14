9557ee937ce9f6c0172d7e48734ec2ea9c45f168
Patty Hung <pattyhung72@gmail.com>
2023-03-15 04:22:02 +1100

    Complete the Link function of the navbar
    Made navbar responsive

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react"
 import Home from "./pages/home/Home"
 import Contactus from "./pages/contactus/Contactus";
 import Navbar from "./pages/Navbar";
-import { Routes, Route} from "react-router-dom";
+import { Link, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
@@ -10,10 +10,10 @@ export default function App() {
       <main>
         <Navbar />
       </main>
-
+      
       <Routes>
-        <Route path = "/" element = {<Home />} />
-        <Route path = "/contactus" element = {<Contactus />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/contactus" element={<Contactus />} />
       </Routes>
     </>
   );
--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,21 +1,25 @@
 import React from "react";
 import { Link, Route, Routes } from "react-router-dom";
+import Home from "./home/Home";
+import Contactus from "./contactus/Contactus";
 
-
-export default function Navbar(){
-    return(
+export default function Navbar() {
+    return (
         <>
-            <navbar className = "flex bg-beige px-10 h-20 items-center w-full">
+            <navbar className="flex bg-beige px-10 h-20 items-center w-full">
                 <h1 className="text-4xl font-bold mr-auto">JENG JYI</h1>
                 <ul className="flex text-lg">
-                    <li className="mr-4"><Link to = "/"></Link>Home</li>
-                    <li className="mr-4"><Link to = "/aboutus"></Link>About us</li>
-                    <li className="mr-4"><Link to = "/contactus"></Link>Contact us</li>
-                    <li className="mr-4"><Link to = "/product"></Link>Product</li>
+                    <Link to="/" className="mr-4">Home</Link>
+                    <Link to="/aboutus" className="mr-4">About us</Link>
+                    <Link to="/contactus" className="mr-4">Contact us</Link>
+                    <Link to="/product" className="mr-4">Product</Link>
+                    
                 </ul>
             </navbar>
 
             
+
+
         </>
     )
 }
