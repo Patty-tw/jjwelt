fbf8cb362310c3811d1cfbca7298d6ceacf11a4f
Patty Hung <pattyhung72@gmail.com>
2023-03-15 03:09:31 +1100

    Contact us
    Build up contactus page

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link, Route, Routes } from "react-router-dom";
-import Home from "./home/Home";
+
 
 export default function Navbar(){
     return(
--- src/pages/contactus/Contactus.js
+++ src/pages/contactus/Contactus.js
@@ -1,19 +1,11 @@
 import React from "react"
 import Map from "./Map"
-import address from "../data/address"
+
 
 export default function Contactus(){
-    const details = address.map(detail => {
-        return (
-            <Map 
-                key = {detail.id}
-                {...detail}
-            />
-        )
-    }) 
     return (
-        <>
-            {details}
+        <>  
+            <Map/>
         </>
     )
 }
