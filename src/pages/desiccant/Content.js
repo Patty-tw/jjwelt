e708af72285a565f8591e650949c6e364ffd304b
Patty Hung <pattyhung72@gmail.com>
2023-03-22 05:30:58 +1100

    Update the look of about us page
    Reorganised the items in about us page

--- src/pages/desiccant/Content.js
+++ src/pages/desiccant/Content.js
@@ -3,7 +3,6 @@ import React from "react";
 export default function Content(){
     return (
         <div className="grid-rows-2"> 
-            <img />
         </div>
     )
 }
--- src/pages/leatherwelt/LeatherHero.js
+++ src/pages/leatherwelt/LeatherHero.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function LeatherHero(){
     return (
         <div className="relative">
-            <img src= "./images/shoe-banner.jpg" alt = "shoe-banner" className=""/>
+            <img src= "./images/shoe-banner.jpg" alt = "shoe-banner"/>
             <h1 className="w-full absolute text-4xl font-semibold text-center text-white top-1/2 ">The art of shoemaking, in every welt we create</h1>
         </div>   
     )
