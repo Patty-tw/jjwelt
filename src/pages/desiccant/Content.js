1ed5662f5ce983ac459f60e6c589b80c06f39efa
Patty Hung <pattyhung72@gmail.com>
2023-03-29 21:45:32 +1100

    Update the layout of desiccant page
    Update the maximum length of the content so the paragraph does not get too wide

--- src/pages/desiccant/Content.js
+++ src/pages/desiccant/Content.js
@@ -7,14 +7,14 @@ export default function Content() {
                 <img src="https://picsum.photos/500?random=6" alt="dessicant" className="mx-auto"/>
                 <div className="my-auto">
                     <h1 className="text-3xl">Raw Material</h1>
-                    <p className="">Our silica gel is Directive 2011/65/EU certified and does not contain Lead, Mercury, Cadmium and hazardous materials set by RoHS.</p>
+                    <p className="w-3/4">Our silica gel is Directive 2011/65/EU certified and does not contain Lead, Mercury, Cadmium and hazardous materials set by RoHS.</p>
                 </div>
             </div>
             <div className="grid grid-cols-2">
                 <img src="https://picsum.photos/500?random=7" alt="dessicant" className="mx-auto"/>
                 <div className="my-auto mx-auto">
                     <h1 className="text-3xl">Various sizes and packing</h1>
-                    <p className="">Sizes: We are able to produce sizes ranging from 1g, 2g, 3G, 4G, 5g, 6g, 10g, 20g, 30g, 50g, 100g, 200g, 500g, 1000g.
+                    <p className="w-3/4">Sizes: We are able to produce sizes ranging from 1g, 2g, 3G, 4G, 5g, 6g, 10g, 20g, 30g, 50g, 100g, 200g, 500g, 1000g.
 
                         Packing material: Compound paper, filter paper, non-woven paper.
 
