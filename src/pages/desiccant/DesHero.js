f5828235d3c20abfc90b95874ab3841591fcb8ba
Patty Hung <pattyhung72@gmail.com>
2023-03-30 00:54:51 +1100

    Change the design of banner
    Remove w-full in all banner

--- src/pages/desiccant/DesHero.js
+++ src/pages/desiccant/DesHero.js
@@ -4,7 +4,7 @@ export default function DesHero(){
     return (
         <div className="relative mb-10">
             <img src= "./images/plastic-banner.jpg" alt = "plastic-banner" className="w-full"/>
-            <h1 className="w-full absolute text-4xl font-semibold text-center text-white top-1/2 ">Desiccant</h1>
+            <h1 className="absolute text-4xl font-semibold text-center text-white top-1/2 ">Desiccant</h1>
         </div>
     )
 }
