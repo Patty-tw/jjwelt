6c3deb6d0e61c1273cdb8ac8e39d334d4cac9f24
Patty Hung <pattyhung72@gmail.com>
2023-03-29 21:47:58 +1100

    Update the layout of leather welt page

--- src/pages/leatherwelt/Content.js
+++ src/pages/leatherwelt/Content.js
@@ -7,14 +7,14 @@ export default function Content() {
                 <img src="https://picsum.photos/500?random=1" alt="dessicant" className="mx-auto" />
                 <div className="my-auto">
                     <h1 className="text-3xl">Leather Welt</h1>
-                    <p className="">Classic welt made from the finest selection of leather. The premium look and feel is an essential element in the overall quality of high-end dress shoes and boots.</p>
+                    <p className="w-3/4">Classic welt made from the finest selection of leather. The premium look and feel is an essential element in the overall quality of high-end dress shoes and boots.</p>
                 </div>
             </div>
             <div className="grid grid-cols-2">
                 <img src="https://picsum.photos/500?random=2" alt="dessicant" className="mx-auto" />
-                <div className="my-auto mx-auto">
+                <div className="my-auto">
                     <h1 className="text-3xl">Leatherboard Welt (Bonded Leather)</h1>
-                    <p className="">Leatherboard welt is a  economic alternative to leather welt. The texture and feel closely mimics that of leather counterpart.</p>
+                    <p className="w-3/4">Leatherboard welt is a  economic alternative to leather welt. The texture and feel closely mimics that of leather counterpart.</p>
                 </div>
             </div>
         </div>
