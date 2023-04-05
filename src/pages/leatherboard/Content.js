03e88f1637a3f4e33254366510f50ae54ae3b1a1
Patty Hung <pattyhung72@gmail.com>
2023-04-05 23:47:41 +1000

    Update layout
    update the layout between  images and content

--- src/pages/leatherboard/Content.js
+++ src/pages/leatherboard/Content.js
@@ -5,14 +5,14 @@ export default function Content() {
         <div className="grid grid-rows-4 gap-5">
             <div className="grid grid-cols-2">
                 <img src="https://picsum.photos/500?random=5" alt="dessicant" className="mx-auto" />
-                <div className="my-auto ">
+                <div className="my-auto sm:ml-5">
                     <h1 className="text-3xl">Introduction of leatherboard</h1>
                     <p className="w-3/4">Extrusion</p>
                 </div>
             </div>
             <div className="grid grid-cols-2">
                 <img src="https://picsum.photos/500?random=1" alt="dessicant" className="mx-auto" />
-                <div className="my-auto ">
+                <div className="my-auto sm:ml-5">
                     <h1 className="text-3xl">Premium bonded leather material sourced from Italy and Germany</h1>
                     <p className="w-3/4">Raw material is made from a mixture of leather scrap and other proprietary mixture
                         to create a leather-like feel. <br />
@@ -21,7 +21,7 @@ export default function Content() {
             </div>
             <div className="grid grid-cols-2">
                 <img src="https://picsum.photos/500?random=2" alt="dessicant" className="mx-auto" />
-                <div className="my-auto">
+                <div className="my-auto sm:ml-5">
                     <h1 className="text-3xl">
                         Precision machines imported from Italy</h1>
                     <p className="w-3/4">As with our leather welts, all bonded leather welt are manufactured and process with the same set of machines 100% from Italy.</p>
@@ -29,7 +29,7 @@ export default function Content() {
             </div>
             <div className="grid grid-cols-2">
                 <img src="https://picsum.photos/500?random=3" alt="dessicant" className="mx-auto" />
-                <div className="my-auto">
+                <div className="my-auto sm:ml-5">
                     <h1 className="text-3xl">
                         Spray painted to any color and finish</h1>
                     <p className="w-3/4">Our experience technicians are able to spray paint any color and effect including polka-dots, antiqued and faux-leather.</p>
@@ -37,7 +37,7 @@ export default function Content() {
             </div>
             <div className="grid grid-cols-2">
                 <img src="https://picsum.photos/500?random=4" alt="dessicant" className="mx-auto" />
-                <div className="my-auto">
+                <div className="my-auto sm:ml-5">
                     <h1 className="text-3xl">Customize to any design and dimensions</h1>
                     <p className="w-3/4">Great alternative for customers looking for leather-like premium feel at a lower price.</p>
                 </div>
