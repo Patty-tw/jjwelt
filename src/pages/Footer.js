0ae8c9a626319944b971ed641b3eb0d4bb10a6cc
Patty Hung <pattyhung72@gmail.com>
2023-04-06 02:51:44 +1000

    Adjust the footer
    Adjust the size of the logo
    Adjust the padding

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 export default function Footer() {
     return (
         <footer>
-            <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+            <div className="w-full max-w-screen-xl mx-auto md:py-8">
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <Link to="/" className="flex items-center mb-4 sm:mb-0">
-                        <img src="./images/jjwelt-logo.png" className="h-20 mr-3" />
+                        <img src="./images/jjwelt-logo.png" className="h-10 mr-3" />
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Jeng Jyi</span>
                     </Link>
 
