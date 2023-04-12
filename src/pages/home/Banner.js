aa90178f0dedc13211b52c213273e976679277d4
Patty Hung <pattyhung72@gmail.com>
2023-04-13 04:27:56 +1000

    Added testimonial on home page

--- src/pages/home/Banner.js
+++ src/pages/home/Banner.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Banner(){
     return(
         <div className="relative">
-            <img src= "./images/shoe-background-1.jpg" alt = "shoe-banner" className="w-full  object-cover "/>
+            <img src= "./images/shoe-background-1.jpg" alt = "shoe-banner" className="w-full object-cover aspect-video"/>
             <h1 className="absolute lg:text-6xl md:text-4xl font-semibold top-10 left-10">
                 Quality Leather Welts <br />
                 and Components</h1>
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Gallery from "./Gallery"
 import Banner from "./Banner"
+import Testimonial from "./Testimonial"
 
 export default function Home() {
     return (
         <div>
             <Banner />
             <Gallery />
+            <Testimonial />
         </div>
 
 
