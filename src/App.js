6bc82aefc2ce93e5d52b47a4a71356a0d57030d2
Patty Hung <pattyhung72@gmail.com>
2023-04-13 03:08:41 +1000

    Create ScrollToTop function in App.js
    Whenever the route changes, the page will start from the top

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react"
+import { useLayoutEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/home/Home"
 import Navbar from "./pages/Navbar";
 import Desiccant from "./pages/desiccant/Desiccant";
@@ -9,10 +11,17 @@ import StackedLeather from "./pages/stackedleather/StackedLeather";
 import LeatherBoard from "./pages/leatherboard/LeatherBoard";
 import LeatherWelt from "./pages/leatherwelt/LeatherWelt";
 import Outsole from "./pages/outsole/Outsole";
-import { Routes, Route } from "react-router-dom";
 import Footer from "./pages/Footer";
 
+
+
 export default function App() {
+  const { pathname } = useLocation();
+
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
   return (
 
     <main>
