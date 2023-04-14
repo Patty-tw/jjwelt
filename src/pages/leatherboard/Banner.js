a28cf5192a1f617be3e3c944189144b9e96d43f2
Patty Hung <pattyhung72@gmail.com>
2023-04-14 20:52:18 +1000

    Photo updates after deployment

--- src/pages/leatherboard/Banner.js
+++ src/pages/leatherboard/Banner.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Banner(){
     return(
         <div className="relative mb-10">
-            <img src= "./images/kilian-seiler-unsplash.jpg" alt = "shoe-banner" className="w-full"/>
+            <img src= "./jjwelt/images/kilian-seiler-unsplash.jpg" alt = "shoe-banner" className="w-full"/>
             <h1 className="absolute lg:text-6xl md:text-4xl font-semibold top-10 left-10 text-white">
                 Toughness Meets Practicality: <br />
                 Durable Plastic Welts for <br />
