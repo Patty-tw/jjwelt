f1c09288f5655c4902f5e95f45e8bf3778a46541
Patty Hung <pattyhung72@gmail.com>
2023-04-14 23:41:03 +1000

    Reset the path for each photo

--- src/pages/desiccant/DesHero.js
+++ src/pages/desiccant/DesHero.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function DesHero(){
     return (
         <div className="relative mb-10">
-            <img src= "./jjwelt/images/plastic-banner.jpg" alt = "plastic-banner" className="w-full"/>
+            <img src= "./images/plastic-banner.jpg" alt = "plastic-banner" className="w-full"/>
             <h1 className="absolute text-4xl font-semibold text-center text-white top-1/2 ">Desiccant</h1>
         </div>
     )
--- src/pages/leather/LeatherHero.js
+++ src/pages/leather/LeatherHero.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function LeatherHero() {
     return (
         <div className="relative mb-10">
-            <img src="./jjwelt/images/james-woods-unsplash.jpg" alt="shoe-banner" className="w-full h-450 object-cover" />
+            <img src="./images/james-woods-unsplash.jpg" alt="shoe-banner" className="w-full h-450 object-cover" />
             <h1 className="absolute text-5xl font-semibold text-white right-10 top-10 ">
                 The art of shoemaking, <br />
                 in every welt we create</h1>
