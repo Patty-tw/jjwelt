a8a1dc1cded2d947c0c164450e2255093b9dd274
Patty Hung <pattyhung72@gmail.com>
2023-04-15 23:45:32 +1000

    Update Navbar
    Use the reusable Contact us component instead of hardcoded

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -98,7 +98,7 @@ export default function Navbar() {
                         className="mr-4 bg-teal-700 text-white px-4 py-2 rounded-md ml-5"
                         onClick={() => setIsOpen(true)} >Contact us
                     </button>
-                    <Contactus />
+                    <Contactus open={isOpen} onClose={() => setIsOpen(false)} />
                 </ul>
             </nav>
         </>
