4247d5e0084131dce5866ba4c99cec84c214c6cb
Patty Hung <pattyhung72@gmail.com>
2023-04-29 18:07:20 +1000

    Update design of navbar
    Added dropdown arrow next to Product

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -2,8 +2,8 @@ import React from "react";
 import { useState } from 'react'
 import { Link } from "react-router-dom";
 import Contactus from "./Contactus";
-import { menuData } from "./navbar/menuData";
 import { Menu, MenuItem, MenuButton, SubMenu } from '@szhsin/react-menu';
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import '@szhsin/react-menu/dist/index.css';
 import '@szhsin/react-menu/dist/transitions/slide.css';
 
@@ -11,20 +11,17 @@ export default function Navbar() {
 
     const [isOpen, setIsOpen] = useState(false)
 
-    // const menuItems = menuData.map(menu => {
-    //     return <Menu key={menu.id} title={menu.title} url={menu.url} />
-    // })
-
-
     return (
         <>
             <nav className="flex bg-neutral-400 px-10 h-20 items-center w-full">
                 <Link to="/" className="text-4xl font-bold mr-auto">JENG JYI</Link>
                 <ul className="flex text-lg items-center">
                     <Link to="/" className="mr-4">Home</Link>
-                    <Link to="/aboutus" className="mr-4">About us</Link>
-                    <Menu menuButton={<MenuButton>Product</MenuButton>}>
-                        <SubMenu label= {<Link to="/leather" className="mr-4">Welting</Link>} >
+                    <Menu menuButton={
+                        <MenuButton className= "inline-flex justify-center" >Product
+                            <ChevronDownIcon className="mt-1 -mr-1 h-5 w-5" aria-hidden="true" />
+                        </MenuButton>}  >
+                        <SubMenu label={<Link to="/leather" className="mr-4">Welting</Link>} >
                             <MenuItem>
                                 <Link to="/leather-welt" className="mr-4">Leather Welt</Link>
                             </MenuItem>
@@ -45,10 +42,9 @@ export default function Navbar() {
                             <MenuItem>
                                 <Link to="/desiccant" className="mr-4">Dessicants</Link>
                             </MenuItem>
-
                         </SubMenu>
-
                     </Menu>
+                    <Link to="/aboutus" className="mr-4 ml-4">About us</Link>
                     <button
                         className="mr-4 bg-teal-700 text-white px-4 py-2 rounded-md ml-5"
                         onClick={() => setIsOpen(true)} >Contact us
