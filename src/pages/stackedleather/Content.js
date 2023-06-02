158ecdbb8033773ae2ccc1b502e0bf107ffa3407
Patty Hung <pattyhung72@gmail.com>
2023-06-02 16:41:10 +1000

    Mobile friendly - stacked leather

--- src/pages/stackedleather/Content.js
+++ src/pages/stackedleather/Content.js
@@ -3,20 +3,13 @@ import React from "react";
 export default function Content() {
   return (
     <div className="grid md:grid-rows-4 md:gap-5 md:px-5 leading-relaxed">
-      <div
-        className="md:grid md:grid-cols-2 
-      "
-      >
+      <div className="md:grid md:grid-cols-2">
         <img
           src="https://picsum.photos/500?random=1"
           alt="raw material of stacked leather"
-          className="mx-auto 
-          "
+          className="w-full md:mx-auto px-5"
         />
-        <div
-          className="md:ml-10 
-         justify-items-end p-5 mx-auto"
-        >
+        <div className="md:ml-10 p-5 mx-auto">
           <h1 className="text-2xl pb-2.5">Raw Material</h1>
           <p className="md:w-4/5">
             We start with highest quality vegetable tanned leather sourced
@@ -30,9 +23,9 @@ export default function Content() {
         <img
           src="https://picsum.photos/500?random=2"
           alt="colors of stacked leather"
-          className="mx-auto"
+          className="w-full mx-5 md:mx-auto"
         />
-        <div className="my-auto sm:ml-10">
+        <div className="md:ml-10 p-5 mx-auto">
           <h1 className="text-2xl pb-2.5">Colors</h1>
           <p className="md:w-4/5">
             We stock multiple colors of raw material which includes Black,
@@ -48,9 +41,9 @@ export default function Content() {
         <img
           src="https://picsum.photos/500?random=3"
           alt="customisation of stacked leather"
-          className="mx-auto"
+          className="w-full mx-5 md:mx-auto"
         />
-        <div className="my-auto sm:ml-10">
+        <div className="md:ml-10 p-5 mx-auto">
           <h1 className="text-2xl pb-2.5">Customization</h1>
           <p className="md:w-4/5">
             Our experienced technicians carefully craft the leather pieces base
@@ -65,9 +58,9 @@ export default function Content() {
         <img
           src="https://picsum.photos/500?random=4"
           alt="quality outcome of stacked"
-          className="mx-auto"
+          className="w-full mx-5 md:mx-auto"
         />
-        <div className="my-auto sm:ml-10">
+        <div className="md:ml-10 p-5 mx-auto">
           <h1 className="text-2xl pb-2.5">Quality Outcome</h1>
           <p className="md:w-4/5">
             With our in-house coloring and finishing, our company are able to
