c9157ba85b9af59902beb69fac14b2eb0e6e3447
Patty Hung <pattyhung72@gmail.com>
2023-06-08 16:55:42 +1000

    Fix layout of testimonial

--- src/pages/home/Testimonial.js
+++ src/pages/home/Testimonial.js
@@ -1,16 +1,18 @@
 import React from "react";
 
-export default function Testimonial(){
-    return(
-        <div>
-            <h1 className="text-center md:text-2xl lg:text-4xl mt-10 mb-10">Our Customers</h1>
-            <div className="grid grid-cols-4 mx-auto px-10 text-center"> 
-                <ul className="md:text-lg lg:text-2xl py-5">Dr.Martens</ul>
-                <ul className="md:text-lg lg:text-2xl py-5">Timberland</ul>
-                <ul className="md:text-lg lg:text-2xl py-5">Coach</ul>
-                <ul className="md:text-lg lg:text-2xl py-5">Zara</ul>
-                <ul className="md:text-lg lg:text-2xl py-5">Lacoste</ul>
-            </div>
-        </div>
-    )
+export default function Testimonial() {
+  return (
+    <div>
+      <h1 className="text-center md:text-2xl lg:text-4xl mt-10 mb-10">
+        Our Customers
+      </h1>
+      <div className="grid grid-cols-3 md:grid-cols-4 mx-auto px-10 text-center">
+        <ul className="md:text-lg lg:text-2xl py-5">Dr.Martens</ul>
+        <ul className="md:text-lg lg:text-2xl py-5">Timberland</ul>
+        <ul className="md:text-lg lg:text-2xl py-5">Coach</ul>
+        <ul className="md:text-lg lg:text-2xl py-5">Zara</ul>
+        <ul className="md:text-lg lg:text-2xl py-5">Lacoste</ul>
+      </div>
+    </div>
+  );
 }
