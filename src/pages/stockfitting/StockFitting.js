4406393716e01b4dfcf6792ffb8399d8a00d829d
Patty Hung <pattyhung72@gmail.com>
2023-06-27 13:59:58 +1000

    Update outsole page
    Include women's outsole pic from company

--- src/pages/stockfitting/StockFitting.js
+++ src/pages/stockfitting/StockFitting.js
@@ -8,24 +8,22 @@ export default function StockFitting() {
       <h1 className="text-center text-2xl md:text-[26px] lg:text-[30px] mt-10 mb-10">
         {t("stock_fitting.women")}
       </h1>
-      <div className="grid grid-cols-auto-fit gap-10 p-10 justify-items-center">
-        <img src="https://picsum.photos/500?random=10" alt="outsole" />
-        <img src="https://picsum.photos/500?random=10" alt="outsole" />
-        <img src="https://picsum.photos/500?random=10" alt="outsole" />
-        <img src="https://picsum.photos/500?random=10" alt="outsole" />
-        <img src="https://picsum.photos/500?random=10" alt="outsole" />
-        <img src="https://picsum.photos/500?random=10" alt="outsole" />
-        <img src="https://picsum.photos/500?random=10" alt="outsole" />
-        <img src="https://picsum.photos/500?random=10" alt="outsole" />
+      <div className="grid grid-cols-3 gap-10 p-10 justify-items-center">
+        <img src="./images/20230627_104048.jpg" alt="outsole1" />
+        <img src="./images/20230627_104151.jpg" alt="outsole2" />
+        <img src="./images/20230627_104316.jpg" alt="outsole3" />
       </div>
       <h1 className="text-center text-2xl md:text-[26px] lg:text-[30px] mt-10 mb-10">
         {t("stock_fitting.men")}
       </h1>
-      <div className="grid grid-cols-auto-fit gap-10 p-10 justify-items-center">
+      <div className="grid grid-cols-3 gap-10 p-10 justify-items-center">
         <img src="./images/20230627_085445.jpg" alt="outsole1" />
         <img src="./images/20230627_090902.jpg" alt="outsole2" />
         <img src="./images/20230627_090320.jpg" alt="outsole3" />
         <img src="./images/20230627_085430.jpg" alt="outsole4" />
+        <img src="./images/20230627_090504.jpg" alt="outsole5" />
+        <img src="./images/20230627_103832.jpg" alt="outsole6" />
+        <img src="./images/20230627_103702.jpg" alt="outsole7" />
       </div>
     </div>
   );
