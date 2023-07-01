c86632822da9bf8fdcd80f89baaeb9cf7562a04c
Patty Hung <pattyhung72@gmail.com>
2023-07-01 15:05:46 +1000

    Update stacked leather page
    - add gallery section
    - include stacked leather pic from company

---
+++ src/pages/stackedleather/Gallery.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Gallery() {
+  return (
+    <div>
+      <h1 className="text-center text-2xl md:text-[26px] lg:text-[30px] my-[45px]">
+        Gallery
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 p-10 justify-items-center lg:px-10">
+        <img src="./images/S__4309213.jpg" alt="leather-welt-1" />
+        <img src="./images/S__4309215.jpg" alt="leather-welt-3" />
+        <img src="./images/S__4309216.jpg" alt="leather-welt-5" />
+        <img src="./images/S__4309217.jpg" alt="leather-welt-5" />
+        <img src="./images/S__4309218.jpg" alt="leather-welt-5" />
+        <img src="./images/S__4309219.jpg" alt="leather-welt-5" />
+        <img src="./images/S__4309220.jpg" alt="leather-welt-5" />
+        <img src="./images/S__4309221.jpg" alt="leather-welt-5" />
+        <img src="./images/S__4309222.jpg" alt="leather-welt-5" />
+        <img src="./images/S__4309223.jpg" alt="leather-welt-5" />
+      </div>
+    </div>
+  );
+}
--- src/pages/stackedleather/StackedLeather.js
+++ src/pages/stackedleather/StackedLeather.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "./Hero";
 import Content from "./Content";
+import Gallery from "./Gallery";
 
 export default function PlasticWelt() {
   return (
     <>
       <Hero />
       <Content />
+      <Gallery />
     </>
   );
 }
