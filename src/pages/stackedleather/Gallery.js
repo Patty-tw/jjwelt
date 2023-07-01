21314c53ee70385d6673f13817a03e1758f67773
Patty Hung <pattyhung72@gmail.com>
2023-07-01 16:40:47 +1000

    Update stacked leather
    Include more png files

--- src/pages/stackedleather/Gallery.js
+++ src/pages/stackedleather/Gallery.js
@@ -10,19 +10,84 @@ export default function Gallery() {
         <img src="./images/S__4309213.jpg" alt="leather-welt-1" />
         <img src="./images/S__4309215.jpg" alt="leather-welt-3" />
         <img src="./images/S__4309216.jpg" alt="leather-welt-5" />
-        <img src="./images/UEXxRMBtkyUPBie.png" alt="leather-welt-5" />
-        <img src="./images/VkVFEpxEJDYySSP.png" alt="leather-welt-5" />
-        <img src="./images/VewIPOnXQZpNfzB.png" alt="leather-welt-5" />
-        <img src="./images/S__4309217.jpg" alt="leather-welt-5" />
+        <img src="./images/S__4309217.jpg" alt="leather-welt-4" />
         <img src="./images/S__4309218.jpg" alt="leather-welt-5" />
-        <img src="./images/S__4309219.jpg" alt="leather-welt-5" />
-        <img src="./images/S__4309220.jpg" alt="leather-welt-5" />
-        <img src="./images/S__4309221.jpg" alt="leather-welt-5" />
-        <img src="./images/S__4309222.jpg" alt="leather-welt-5" />
-        <img src="./images/S__4309223.jpg" alt="leather-welt-5" />
-        <img src="./images/S__4309075.jpg" alt="leather-welt-5" />
-        <img src="./images/S__4309076.jpg" alt="leather-welt-5" />
-        <img src="./images/S__4309077.jpg" alt="leather-welt-5" />
+        <img src="./images/S__4309219.jpg" alt="leather-welt-6" />
+        <img src="./images/S__4309220.jpg" alt="leather-welt-7" />
+        <img src="./images/S__4309221.jpg" alt="leather-welt-8" />
+        <img src="./images/S__4309222.jpg" alt="leather-welt-9" />
+        <img src="./images/S__4309223.jpg" alt="leather-welt-10" />
+        <img src="./images/S__4309075.jpg" alt="leather-welt-11" />
+        <img src="./images/S__4309076.jpg" alt="leather-welt-12" />
+        <img src="./images/S__4309077.jpg" alt="leather-welt-13" />
+        <br />
+        <img
+          src="./images/UEXxRMBtkyUPBie.png"
+          alt="leather-welt-1"
+          className="aspect-square object-center object-contain"
+        />
+        <img
+          src="./images/VkVFEpxEJDYySSP.png"
+          alt="leather-welt-2"
+          className="aspect-square object-center object-contain"
+        />
+        <img
+          src="./images/VewIPOnXQZpNfzB.png"
+          alt="leather-welt-3"
+          className="aspect-square object-center object-contain"
+        />
+        <img
+          src="./images/CnNIanpVejakSBj.png"
+          alt="leather-welt-4"
+          className="aspect-square object-center object-contain"
+        />
+        <img
+          src="./images/CRPOwLMqiOOuSmJ.png"
+          alt="leather-welt-5"
+          className="aspect-square object-center object-contain"
+        />
+        <img
+          src="./images/iTDWxdvHLpmkZjb.png"
+          alt="leather-welt-6"
+          className="aspect-square object-center object-contain"
+        />
+
+        <img
+          src="./images/IXzSRZycobRpOOV.png"
+          alt="leather-welt-7"
+          className="aspect-square object-center object-contain"
+        />
+        <img
+          src="./images/EOgCbjABBeWsTEx.png"
+          alt="leather-welt-8"
+          className="aspect-square object-center object-contain"
+        />
+        <img
+          src="./images/eBgxvBjewhHBbyx.png"
+          alt="leather-welt-9"
+          className="aspect-square object-center object-contain"
+        />
+
+        <img
+          src="./images/OcvqIQOYngRFAqA.png"
+          alt="leather-welt-10"
+          className="aspect-square object-center object-contain"
+        />
+        <img
+          src="./images/grPZLOIxvhRXkmb.png"
+          alt="leather-welt-11"
+          className="aspect-square object-center object-contain"
+        />
+        <img
+          src="./images/tezaREhTDilgpgg.png"
+          alt="leather-welt-12"
+          className="aspect-square object-center object-contain"
+        />
+        <img
+          src="./images/YALKPDkDxIpwpxE.png"
+          alt="leather-welt-13"
+          className="aspect-square object-center object-contain"
+        />
       </div>
     </div>
   );
