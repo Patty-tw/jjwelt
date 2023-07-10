200af461152c1c420e96094fd9ee93caa86943ae
Patty Hung <pattyhung72@gmail.com>
2023-07-10 15:43:47 +1000

    Make stock fitting page mobile friendly

--- src/pages/stockfitting/Machine.js
+++ src/pages/stockfitting/Machine.js
@@ -6,8 +6,8 @@ export default function Machine() {
       <h1 className="text-center text-2xl md:text-[26px] lg:text-[30px] mt-10 mb-10">
         Machine
       </h1>
-      <div className="grid grid-cols-4 gap-10 lg:px-10">
-        <div className="col-span-2 h-[550px] ">
+      <div className="md:grid md:grid-cols-4 gap-10 px-10">
+        <div className="md:col-span-2 md:h-[550px] mb-10 md:mb-0">
           <h1 className="text-xl">CNC 自動切割挖槽機</h1>
           <video controls className="h">
             <source src="./images/machine-1.mp4" type="video/mp4"></source>
@@ -15,13 +15,13 @@ export default function Machine() {
         </div>
         <div>
           <h1 className="text-xl">半自動磨邊機</h1>
-          <video controls className="h-[525px]">
+          <video controls className="md:h-[525px] mb-10 md:mb-0">
             <source src="./images/machine-2.mp4" type="video/mp4"></source>
           </video>
         </div>
         <div>
           <h1 className="text-xl">自動磨邊機</h1>
-          <video controls className="h-[525px]">
+          <video controls className="md:h-[525px] mb-10 md:mb-0">
             <source src="./images/machine-3.mp4" type="video/mp4"></source>
           </video>
         </div>
